4c0a53fb3922069251b979b221411cff2fe96d23
Hamzata MAREGA <hamshakour93@gmail.com>
2021-01-22 17:49:30 +0100

    reload features

--- src/app/places/discover/place-detail/place-detail.module.ts
+++ src/app/places/discover/place-detail/place-detail.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { PlaceDetailPageRoutingModule } from './place-detail-routing.module';
-
 import { PlaceDetailPage } from './place-detail.page';
+import {CreateBookingComponent} from '../../../bookings/create-booking/create-booking.component';
+import {RouterModule, Routes} from '@angular/router';
 
+
+const routes: Routes = [
+  {
+    path: '',
+    // @ts-ignore
+    componenent: PlaceDetailPage
+  }
+];
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PlaceDetailPageRoutingModule
+    PlaceDetailPageRoutingModule,
+      RouterModule.forChild(routes),
   ],
-  declarations: [PlaceDetailPage]
+  declarations: [PlaceDetailPage, CreateBookingComponent],
+  entryComponents: [CreateBookingComponent]
 })
 export class PlaceDetailPageModule {}
