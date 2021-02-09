4f04f9508626d6ec49ab2b9d06b06fed870dd49c
Hamzata MAREGA <hamshakour93@gmail.com>
2021-02-09 20:28:01 +0100

    Adding a new component and finishing a section 9

--- src/app/places/offers/offers.module.ts
+++ src/app/places/offers/offers.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import {RouterModule, Routes} from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { OffersPageRoutingModule } from './offers-routing.module';
 
 import { OffersPage } from './offers.page';
+import {OfferItemComponent} from './offer-item/offer-item.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: OffersPage
+  }
+];
+
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    RouterModule.forChild(routes),
     OffersPageRoutingModule
   ],
-  declarations: [OffersPage]
+  declarations: [OffersPage, OfferItemComponent]
 })
 export class OffersPageModule {}
